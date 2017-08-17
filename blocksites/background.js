82752d599b625cf1c478a717261ebe779fec6455
Jagadeesh Posni <jagadeeshposni@gmail.com>
2017-08-17 23:22:45 +0530

    Actual blocking logic in background page

--- blocksites/background.js
+++ blocksites/background.js
@@ -2,21 +2,25 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         //alert("hi");
 		var siteNameOpened = request.siteURL;
-        chrome.tabs.query({}, function(tabs) {
-            //alert("hi");
-            for (var i = 0; i < tabs.length; i++) {
-                var tabURL = tabs[i].url;
-                //alert(tabURL);
-                if (tabURL.includes(siteNameOpened)) {
-                    chrome.tabs.remove(tabs[i].id);
-                }
-            }
-        });
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.greeting == "hello")
-            sendResponse({
-                farewell: "goodbye"
-            });
+		//alert("URL of opened site is " + siteNameOpened);
+		 chrome.storage.sync.get('siteKeyIds', function (result) {
+            console.log(result.siteKeyIds);
+            for (var i = 0; i < result.siteKeyIds.length; i++) {
+            	//alert(result.siteKeyIds[i].siteName);
+            	if(siteNameOpened.includes(result.siteKeyIds[i].siteName)){
+					chrome.tabs.query({}, function(tabs) {
+						for (var i = 0; i < tabs.length; i++) {
+							var tabURL = tabs[i].url;
+							//alert(tabURL);
+							if (tabURL.includes(siteNameOpened)) {
+								chrome.tabs.remove(tabs[i].id);
+								alert("that site was blocked by yourself");
+							}
+						}
+					});
+
+				}
+			}
+		});
+		
     });
