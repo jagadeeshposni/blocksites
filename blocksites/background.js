978ec62a62e56e5b3a819a75f60d0e3b1453fd69
Jagadeesh Posni <jagadeeshposni@gmail.com>
2017-08-21 17:29:26 +0530

    Adding icon to the extension

--- blocksites/background.js
+++ blocksites/background.js
@@ -1,17 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        //alert("hi");
 		var siteNameOpened = request.siteURL;
-		//alert("URL of opened site is " + siteNameOpened);
 		 chrome.storage.sync.get('siteKeyIds', function (result) {
             console.log(result.siteKeyIds);
             for (var i = 0; i < result.siteKeyIds.length; i++) {
-            	//alert(result.siteKeyIds[i].siteName);
-            	if(siteNameOpened.includes(result.siteKeyIds[i].siteName)){
+            	if(siteNameOpened.includes(result.siteKeyIds[i].siteName + ".com")){
 					chrome.tabs.query({}, function(tabs) {
 						for (var i = 0; i < tabs.length; i++) {
 							var tabURL = tabs[i].url;
-							//alert(tabURL);
 							if (tabURL.includes(siteNameOpened)) {
 								chrome.tabs.remove(tabs[i].id);
 								alert("That site was blocked by yourself!");
